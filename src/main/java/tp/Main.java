bea2affb9ff127c00b365d56e18f2768fe79bd0c
yungNita <yung.nita1306@gmail.com>
2025-04-19 21:40:25 +0700

    TP1

--- src/main/java/tp/Main.java
+++ src/main/java/tp/Main.java
@@ -1,10 +1,18 @@
 package tp;
 
+/**
+ * Main class for TP1 application.
+ */
 public final class Main {
-    private Main() {}
+    private Main() {
+        // Private constructor to prevent instantiation
+    }
+
+    /**
+     * Entry point of the application.
+     * @param args Command-line arguments.
+     */
     public static void main(String[] args) {
         System.out.println("TP1 Application Running!");
     }
-
-    
 }
